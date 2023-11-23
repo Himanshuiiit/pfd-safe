cd8873ced4313bf817b9e378e543fff258839567
Himanshu Kumar Verma <76449638+Himanshuiiit@users.noreply.github.com>
2023-11-23 17:09:24 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import PDFComponent from "../components/PDFComponent";
 
 export default async function Home() {
   const { encryptedPassword } = await (
-    await fetch("https://pfd-safe.vercel.app/password", {
+    await fetch("http://localhost:3000/password", {
       method: "GET",
       cache: "no-cache",
     })
